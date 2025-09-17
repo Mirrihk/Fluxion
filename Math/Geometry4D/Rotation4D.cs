7d2049cfc077d83f550d9650b6e72b3d50f2bfdc
Mirrihk <sebas6721@gmail.com>
2025-09-17 02:55:52 -0400

    Rotation4D

---
+++ Math/Geometry4D/Rotation4D.cs
@@ -0,0 +1,28 @@
+/*
+|--------------------------------------------------------------------------------|
+|           File : Fluxion.Math/Geometry4D/Rotation4D.cs                         |
+|--------------------------------------------------------------------------------|
+*/
+
+using System;
+using System.Numerics;
+
+
+namespace Fluxion.Math.Geometry4D
+{
+    /// <summary>
+    /// 4D plane rotations. Each Method returns a Matrix4x4 rotating in the name plane.
+    /// </summary>
+
+    public static class Rotation4D
+    {
+        public static Matrix4x4 RotationXY                
+        public static Matrix4x4 RotationXZ            
+        public static Matrix4x4 Rotation            
+        public static Matrix4x4 Rotation            
+        public static Matrix4x4 Rotation            
+        public static Matrix4x4 Rotation                
+    }
+}
+
+
